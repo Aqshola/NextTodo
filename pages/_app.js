85266c6e2016cae323cfe0539b9a85e342c12055
Aqshola <aqsholafid2@gmail.com>
2021-02-08 16:04:24 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import "../styles/tailwind.css";
-import { Provider } from "react-redux";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 import { wrapper } from "../redux/store";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default wrapper.withRedux(MyApp);
